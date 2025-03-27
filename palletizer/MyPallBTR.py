a063814b8a981ea033ea0f1072f79f081f1b760d
BabyTigers-R <robocup@vega.elec.ryukoku.ac.jp>
2025-03-27 09:38:21 +0100

    Shohei updated this file for JapanOpen2025.

--- palletizer/MyPallBTR.py
+++ palletizer/MyPallBTR.py
@@ -19,58 +19,117 @@ serialBaud = "1000000"
 class MycobotBTR(object):
     def __init__(self):
         self.mycobot = None
+        self.angle_tolerance = 3
 
     def run(self, mode:str):
         self.connect_mycobot()
         self.mycobot.power_on()
         self.send_color("green")
 
-        f = open("/home/er/yasuda_pall/jo2025/motion/{}.txt".format(mode), 'r')
+        f = open("/home/er/git/rcll/palletizer/motion/{}.txt".format(mode), 'r')
         self.data = f.read()
-        self.move()
+        self.playback()
         f.close()
 
         self.disconnect_mycobot()
 
-    def move(self):
+    def playback(self):
         command_list = ["move", "wait", "close", "open"]
         splited_data = self.data.split()
         l = len(splited_data)
+        # pointer to read command data
         p = 0
+
         while p < l:
+            # move
             if splited_data[p] == command_list[0]:
-                self.mycobot.send_angles([float(splited_data[p+1]), float(splited_data[p+2]), float(splited_data[p+3]), float(splited_data[p+4])], int(float(splited_data[p+5])))
+                goal_angles = [float(splited_data[p+1]), float(splited_data[p+2]), float(splited_data[p+3]), float(splited_data[p+4])]
+                self.moveArm(goal_angles, int(float(splited_data[p+5])))
                 p += 6
+            
+            # wait
             elif splited_data[p] == command_list[1]:
                 time.sleep(float(splited_data[p+1]))
                 p += 2
+            
+            # close
             elif splited_data[p] == command_list[2]:
                 self.gripperClose()
+                self.getStatus()
                 p += 1
+            
+            # open
             elif splited_data[p] == command_list[3]:
                 self.gripperOpen()
+                self.getStatus()
                 p += 1
 
+
+    def moveArm(self, goal_angles:list, speed):
+        self.mycobot.send_angles(goal_angles, speed)
+
+        # check current angles
+        start_time = time.time()
+        self.getStatus()
+        errors = [goal_angles[i] - self.angles[i] for i in range(4)]
+        print("Angle errors: ", errors)
+        while (abs(errors[0]) > self.angle_tolerance) or (abs(errors[1]) > self.angle_tolerance) or (abs(errors[2]) > self.angle_tolerance)  or (abs(errors[3]) > self.angle_tolerance):
+            self.getStatus()
+            past_time = time.time() - start_time
+            print(past_time)
+            if past_time > 5:
+                break
+            errors = [goal_angles[i] - self.angles[i] for i in range(4)]
+            print("Angle errors: ", errors)
 
     def gripperOpen(self):
         try:
-            self.mycobot.set_gripper_value(255, 30)
+            self.mycobot.set_gripper_value(20, 30)
+            time.sleep(2)
+            self.mycobot.set_gripper_value(60, 30)
+            time.sleep(2)
+            self.mycobot.set_gripper_value(100, 30)
+            time.sleep(1)
+            self.getStatus()
+            start_time = time.time()
+            while self.gripper_value < 70:
+                self.getStatus()
+                past_time = time.time() - start_time
+                print(past_time)
+                if past_time > 5:
+                    break
         except Exception as e:
             print(e)
             pass
-        time.sleep(0.5)
 
     def gripperClose(self):
         try:
+            self.mycobot.set_gripper_value(80, 30)
+            time.sleep(2)
+            self.mycobot.set_gripper_value(40, 30)
+            time.sleep(2)
             self.mycobot.set_gripper_value(0, 30)
+            time.sleep(1)
+            self.getStatus()
+            start_time = time.time()
+            while self.gripper_value > 30:
+                self.getStatus()
+                past_time = time.time() - start_time
+                print(past_time)
+                if past_time > 5:
+                    break
         except Exception as e:
             print(e)
             pass
-        time.sleep(0.5)
 
     def getStatus(self):
-        print("Angles: ", self.mycobot.get_angles())
-        # print("Radians: ", self.mycobot.get_radians())
+        angles = [*self.mycobot.get_angles()]
+        while len(angles) != 4:
+            angles = [*self.mycobot.get_angles()]
+        self.angles = angles
+        self.gripper_value = self.mycobot.get_gripper_value()
+        print("Angles: ", "{}".format(self.angles))
+        print("Gripper value: ", "{}".format(self.gripper_value))
 
     def motor_on(self):
         self.mycobot.power_on()
