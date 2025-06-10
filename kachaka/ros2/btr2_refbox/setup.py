ec1c58c2d127da215651d140174eb818bcd45644
Wataru Uemura <wataru@kdel.org>
2025-06-10 19:54:44 +0900

    copy btr2_refbox.py from 2025-GermanOpen

---
+++ kachaka/ros2/btr2_refbox/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'btr2_refbox'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    py_modules=['btr2_refbox.btr2_refbox'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Wataru UEMURA',
+    maintainer_email='wataru@kdel.org',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'btr2_refbox = btr2_refbox.btr2_refbox:main'
+        ],
+    },
+)
