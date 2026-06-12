395ba044effe6967c84773d9f3cc151840c2c2fb
Wataru Uemura <wataru@kdel.org>
2026-06-12 18:41:13 +0900

    set ROS_DOMAIN_ID using IP address.

---
+++ myAGV/startup_myAGV.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export ROS_DOMAIN_ID=$(hostname -I | awk '{print $1}' | awk -F. '{print $4}')
+# export PS1="(domain:$ROS_DOMAIN_ID) $PS1"
+echo "ROS_DOMAIN_ID = $ROS_DOMAIN_ID"
+
+# ros2shell
+bash() { :; }
+source /home/er/.local/bin/ros2shell
+unset -f bash
+
+# startup for LiDAR
+# initialization for IO ports
+# enable GPIO 20
+echo 20 | sudo tee /sys/class/gpio/export
+# set output mode
+echo out | sudo tee /sys/class/gpio/gpio20/direction
+# power on
+echo 1 | sudo tee /sys/class/gpio/gpio20/value
+
+source ~/myagv_ros2/install/setup.bash
+ros2 launch myagv_odometry myagv_active.launch.py
+
